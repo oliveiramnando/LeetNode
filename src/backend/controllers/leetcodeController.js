34d0802955a774c6e1c8511c854d6f061ab15e98
oliveiramnando <oliveiramnando@gmail.com>
2026-01-16 21:12:57 -0500

    getUser controller from req.params

---
+++ src/backend/controllers/leetcodeController.js
@@ -0,0 +1,13 @@
+import { LeetCode } from "leetcode-query";
+
+export const getUser = async (req,res) => {
+    try {
+        const { username } = req.params;
+
+        const leetcode = new LeetCode();
+        const user = await leetcode.user(username);
+        return res.json(user);
+    } catch (err) {
+        console.log(err);
+    }
+};
