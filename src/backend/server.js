d307ea082f72df63558827310ac7f5add528bb4e
oliveiramnando <oliveiramnando@gmail.com>
2026-01-29 17:05:57 -0500

    configurations moved

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,21 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import express from "express";
 
+dotenv.config();
+
 import { connectDB } from "./config/db.js";
+import { session } from "./config/session.js";
+import { cors } from "./config/cors.js";
 import authRoutes from "./routes/authRoutes.js"
 import leetcodeRoutes from "./routes/leetcodeRoute.js";
 
-
 const PORT = process.env.PORT || 5050
 const app = express();
+
+app.use(express.json);
+
+app.use(cors); // allows frontend use with cookies
+app.use(session); // cookie-based session
 
 app.get('/api/health', (_,res) => {
     res.json({ok: true});
