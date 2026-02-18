78012f45a0359f6e52f3c75710eec42c572c9a20
oliveiramnando <oliveiramnando@gmail.com>
2026-02-18 15:55:29 -0500

    leetcode username

--- src/backend/routes/leetcodeRoute.js
+++ src/backend/routes/leetcodeRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser } from "../controllers/leetcodeController.js";
+import { getUser, getGithubUsername } from "../controllers/leetcodeController.js";
 
 const router = express.Router();
 
 router.get('/user/:username', getUser);
+// router.get('/github-username', getGithubUsername);
 
 export default router
