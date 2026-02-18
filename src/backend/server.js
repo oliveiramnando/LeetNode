cebd0e70b7f0b705f9aaa20c983d9718a2bdeebe
oliveiramnando <oliveiramnando@gmail.com>
2026-02-18 15:54:50 -0500

    server

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,22 +4,24 @@ import express from "express";
 dotenv.config();
 
 import { connectDB } from "./config/db.js";
-import { session } from "./config/session.js";
-import { cors } from "./config/cors.js";
+import session from "./config/session.js";
+import cors from "./config/cors.js";
 import authRoutes from "./routes/authRoutes.js"
 import leetcodeRoutes from "./routes/leetcodeRoute.js";
 
-const PORT = process.env.PORT || 5050
+const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
 app.use(cors); // allows frontend use with cookies
-app.use(session); // cookie-based session
+// session maybe isn't here
+// app.use(session); // cookie-based session
+
 
 app.get('/api/health', (_,res) => {
     res.json({ok: true});
-})
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/leetcode', leetcodeRoutes);
