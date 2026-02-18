333104ec5478d33b22736a7ce1ae238fdbf88c2c
oliveiramnando <oliveiramnando@gmail.com>
2026-02-18 15:56:49 -0500

    potential auth for leetcode-query

--- src/backend/controllers/leetcodeController.js
+++ src/backend/controllers/leetcodeController.js
@@ -1,12 +1,18 @@
-import { LeetCode } from "leetcode-query";
+import { LeetCode, Credential } from "leetcode-query";
 
 export const getUser = async (req,res) => {
     try {
         const { username } = req.params;
 
-        const leetcode = new LeetCode();
+        const credential = new Credential();
+        await credential.init("YOUR-LEETCODE-SESSION-COOKIE");
+
+        const leetcode = new LeetCode(credential);
+
         const user = await leetcode.user(username);
-        return res.json(user);
+        // const submissions =  await leetcode.submissions({ limit: 10, offset: 0 });
+        // return res.json({user, submissions});
+        return res.json({user});
     } catch (err) {
         console.log(err);
     }
