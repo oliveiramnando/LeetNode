9fca109cd775b3be578930ae13bdc90ae8a4e54d
oliveiramnando <oliveiramnando@gmail.com>
2026-02-19 01:21:51 -0500

    server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,33 +1,26 @@
-import dotenv from "dotenv";
+import "./env.js"; 
+
 import express from "express";
 
-dotenv.config();
-
+import corsMiddleware from "./config/cors.js";
+import sessionMiddleware from "./config/session.js";
 import { connectDB } from "./config/db.js";
-import session from "./config/session.js";
-import cors from "./config/cors.js";
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
 import leetcodeRoutes from "./routes/leetcodeRoute.js";
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
 
-app.use(cors); // allows frontend use with cookies
-// session maybe isn't here
-// app.use(session); // cookie-based session
+app.use(corsMiddleware);
+app.use(sessionMiddleware);
 
+app.get("/api/health", (_, res) => res.json({ ok: true }));
 
-app.get('/api/health', (_,res) => {
-    res.json({ok: true});
-});
-
-app.use('/api/auth', authRoutes);
-app.use('/api/leetcode', leetcodeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/leetcode", leetcodeRoutes);
 
 connectDB().then(() => {
-	app.listen(PORT, () => {
-		console.log('Server started on PORT:', PORT);
-	});
+	app.listen(PORT, () => console.log("Server started on PORT:", PORT));
 });
