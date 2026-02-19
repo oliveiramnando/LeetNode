5afb1ea275bcc95f26a6674c6349a4f1d469b14e
oliveiramnando <oliveiramnando@gmail.com>
2026-02-19 01:19:29 -0500

    routes

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
-
-import { identifier } from '../middlewares/identification.js';
-import { startGithubOAuth, githubCallback, logout } from '../controllers/authController.js';
+import { identifier } from "../middlewares/identification.js";
+import { startGithubOAuth, githubCallback, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get('/github/start', startGithubOAuth);
-router.get('/github/callback', githubCallback);
-router.post('/signout', identifier, logout);
+router.get("/github/start", startGithubOAuth);
+router.get("/github/callback", githubCallback);
+router.post("/signout", identifier, logout);
 
 export default router;
--- src/backend/routes/leetcodeRoute.js
+++ src/backend/routes/leetcodeRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getUser, getGithubUsername } from "../controllers/leetcodeController.js";
+import { getUser, me, getGithubUsername } from "../controllers/leetcodeController.js";
 
 const router = express.Router();
 
+
+router.get('/me', me);
 router.get('/user/:username', getUser);
+
+// router.get('/user/:username', getUser);
+
 // router.get('/github-username', getGithubUsername);
 
 export default router
