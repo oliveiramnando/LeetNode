666165ca38f01b85e5a218090912717b3371c287
oliveiramnando <oliveiramnando@gmail.com>
2026-02-27 19:11:14 -0500

    auth routes test

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,44 +1,40 @@
 import express from "express";
-import { identifier } from "../middlewares/identification.js";
-import { startGithubOAuth, githubCallback, logout, signin, signup } from "../controllers/authController.js";
+import { startGithubOAuth, githubOAuthCallback, signin, signup } from "../controllers/authController.js";
+// import { startGithubOAuth, githubOAuthCallback, logout, signin, signup } from "../controllers/authController.js";
 import User from "../models/User.js";
 
 const router = express.Router();
 
-function requireSession(req, res, next) {
-    if (!req.session?.userId) return res.status(401).json({ message: "Not logged in" });
-    next();
-}
 
-router.get("/me", async (req, res) => {
-    try {
-        if (!req.session?.user) return res.status(401).json({ loggedIn: false });
+// router.get("/me", async (req, res) => {
+//     try {
+//         if (!req.session?.user) return res.status(401).json({ loggedIn: false });
 
-        const dbUser = await User.findById(req.session.userId);
-        if (!dbUser) {
-            // This shouldn't happen - session exists but user not found. Clear session to be safe.
-            req.session.destroy(() => {
-                return res.status(401).json({ loggedIn: false });
-            });
-            return;
-        }
-        return res.json({ 
-            loggedIn: true, 
-            user: {
-                id: dbUser._id,
-                ghUsername: dbUser.ghUsername,
-                githubUrl: dbUser.githubUrl,
-                leetcodeUsername: dbUser.leetcodeUsername ?? null,
-            }
-        });
-    } catch (err) {
-        return res.status(500).json({ message: "Server error" });
-    }
-});
+//         const dbUser = await User.findById(req.session.userId);
+//         if (!dbUser) {
+//             // This shouldn't happen - session exists but user not found. Clear session to be safe.
+//             req.session.destroy(() => {
+//                 return res.status(401).json({ loggedIn: false });
+//             });
+//             return;
+//         }
+//         return res.json({ 
+//             loggedIn: true, 
+//             user: {
+//                 id: dbUser._id,
+//                 ghUsername: dbUser.ghUsername,
+//                 githubUrl: dbUser.githubUrl,
+//                 leetcodeUsername: dbUser.leetcodeUsername ?? null,
+//             }
+//         });
+//     } catch (err) {
+//         return res.status(500).json({ message: "Server error" });
+//     }
+// });
 
 router.get("/github/start", startGithubOAuth);
-router.get("/github/callback", githubCallback);
-router.post("/signout", logout);
+router.get("/github/callback", githubOAuthCallback);
+// router.post("/signout", logout);
 
 router.post("/signup", signup);
 router.post("/signin", signin);
