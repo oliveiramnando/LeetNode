f51bedb864f297c0859b53521c14a0a5167d1aa7
oliveiramnando <oliveiramnando@gmail.com>
2026-02-27 20:51:23 -0500

    routes

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,40 +1,13 @@
 import express from "express";
-import { startGithubOAuth, githubOAuthCallback, signin, signup } from "../controllers/authController.js";
-// import { startGithubOAuth, githubOAuthCallback, logout, signin, signup } from "../controllers/authController.js";
-import User from "../models/User.js";
+import { me, startGithubOAuth, githubOAuthCallback, logout, signin, signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
-
-// router.get("/me", async (req, res) => {
-//     try {
-//         if (!req.session?.user) return res.status(401).json({ loggedIn: false });
-
-//         const dbUser = await User.findById(req.session.userId);
-//         if (!dbUser) {
-//             // This shouldn't happen - session exists but user not found. Clear session to be safe.
-//             req.session.destroy(() => {
-//                 return res.status(401).json({ loggedIn: false });
-//             });
-//             return;
-//         }
-//         return res.json({ 
-//             loggedIn: true, 
-//             user: {
-//                 id: dbUser._id,
-//                 ghUsername: dbUser.ghUsername,
-//                 githubUrl: dbUser.githubUrl,
-//                 leetcodeUsername: dbUser.leetcodeUsername ?? null,
-//             }
-//         });
-//     } catch (err) {
-//         return res.status(500).json({ message: "Server error" });
-//     }
-// });
+router.get("/me", me);
 
 router.get("/github/start", startGithubOAuth);
 router.get("/github/callback", githubOAuthCallback);
-// router.post("/signout", logout);
+router.post("/signout", logout);
 
 router.post("/signup", signup);
 router.post("/signin", signin);
