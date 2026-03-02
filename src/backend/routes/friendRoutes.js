459dc23325acc245d981168daabcd1918337dd3a
oliveiramnando <oliveiramnando@gmail.com>
2026-03-02 12:34:27 -0500

    counts and is-following routes

--- src/backend/routes/friendRoutes.js
+++ src/backend/routes/friendRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { follow, unfollow, getFollowers, getFollowing } from "../controllers/friendController.js";
+import { follow, unfollow, getFriendCounts, isFollowing, getFollowers, getFollowing } from "../controllers/friendController.js";
 
 const router = express.Router();
 
 
 router.post('/:leetcodeUsername/follow', follow);
 router.delete('/:leetcodeUsername/follow', unfollow);
+
+router.get('/counts', getFriendCounts);
+router.get('/is-following/:leetcodeUsername', isFollowing)
 
 router.get('/followers', getFollowers);
 router.get('/following', getFollowing);
