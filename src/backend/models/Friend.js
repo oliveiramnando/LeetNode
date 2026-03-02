b883cec414080adb29c7088b12d8f71b7ac5baca
oliveiramnando <oliveiramnando@gmail.com>
2026-03-01 22:01:50 -0500

    lowercase and index

--- src/backend/models/Friend.js
+++ src/backend/models/Friend.js
@@ -7,20 +7,21 @@ const FriendSchema = new mongoose.Schema({
         required: true,
         index: true
     },
-    following: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        // required: true,
-        index: true
-    },
     leetcodeUsername: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         index: true
     }
 }, {
     timestamps: true
 });
+
+FriendSchema.index(
+    { leetnodeUser: 1, leetcodeUsername: 1 },
+    { unique: true }
+);
 
 const Friend = mongoose.model('Friend', FriendSchema);
 export default Friend;
