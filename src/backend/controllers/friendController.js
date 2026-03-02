d1bfd288d907655945766adca31cfbc92a65d1fe
oliveiramnando <oliveiramnando@gmail.com>
2026-03-01 19:12:59 -0500

    verify leetcoder move; empty list is okay

--- src/backend/controllers/friendController.js
+++ src/backend/controllers/friendController.js
@@ -14,15 +14,9 @@ async function verifyLeetcodeUser(leetcodeUsername) {
 export const follow = async (req,res) => {
     try {
         const { targetLeetcodeUsername } = req.body;
-
-        const existsLeetcoder = verifyLeetcodeUser(targetLeetcodeUsername);
-        if (!existsLeetcoder) return res.status(404),json({ success: false, message: "Leetcoder does not exist"});
-
         const currentUserId = req.session?.userId;
         // const currentUserId = new mongoose.Types.ObjectId(); testing
         const currentLeetcodeUsername = req.session?.leetcodeUsername;
-
-        console.log(currentUserId);
 
         if (!targetLeetcodeUsername) return res.status(400).json({ success: false, message: "Leetcode username not provided" });
         if (!currentUserId) return res.status(400).json({ success: false, message: "Not logged in" });
@@ -32,8 +26,11 @@ export const follow = async (req,res) => {
         const existingFollow = await Friend.findOne({ leetnodeUser: currentUserId, leetcodeUsername: targetLeetcodeUsername })
         if (existingFollow) return res.status(400).json({ success: false, message: "Already following user" });
 
+        const existsLeetcoder = await verifyLeetcodeUser((targetLeetcodeUsername).trim());
+        if (!existsLeetcoder) return res.status(404).json({ success: false, message: "Leetcoder does not exist"});
+
         const follow = new Friend({ leetnodeUser: currentUserId, leetcodeUsername: targetLeetcodeUsername })
-        console.log(follow)
+        // console.log(follow)
 
         await follow.save();
 
@@ -55,10 +52,6 @@ export const follow = async (req,res) => {
 export const unfollow = async (req,res) => {
     try {
         const { targetLeetcodeUsername } = req.body;
-
-        const existsLeetcoder = verifyLeetcodeUser(targetLeetcodeUsername);
-        if (!existsLeetcoder) return res.status(404),json({ success: false, message: "Leetcoder does not exist"});
-
         const currentUserId = req.session?.userId;
         // const currentUserId = new mongoose.Types.ObjectId(); testing
         const currentLeetcodeUsername = req.session?.leetcodeUsername;
@@ -72,9 +65,7 @@ export const unfollow = async (req,res) => {
         if (!existingFollow) return res.status(400).json({ success: false, message: "You are not following this user" });
 
         const unfollow = await Friend.findOneAndDelete({ leetnodeUser: currentUserId, leetcodeUsername: targetLeetcodeUsername })
-        console.log(unfollow)
-
-        // unfollow.save();
+        // console.log(unfollow)
 
         return res.status(200).json({
             success: true,
@@ -96,7 +87,7 @@ export const getFollowers = async (req,res) => {
         if (!currentleetcodeUsername) return res.status(400).json({ succes: false, message: "leetcodeUsername not provided" });
 
         const followers = await Friend.find({ leetcodeUsername: currentleetcodeUsername });
-        if (!followers) return res.status(400).json({ success: false, message: "unable to get fetch user followers" });
+        // if (!followers) return res.status(400).json({ success: false, message: "unable to get fetch user followers" });
 
         // in front end should display only leetNodeuser cause that's whos following them
         return res.status(200).json({
@@ -128,7 +119,7 @@ export const getFollowing = async (req,res) => {
         if (!currentUserId) return res.status(400).json({ succes: false, message: "userId not provided" });
 
         const following = await Friend.find({ leetnodeUser: currentUserId });
-        if (!following) return res.status(400).json({ success: false, message: "unable to get fetch user following" });
+        // if (!following) return res.status(400).json({ success: false, message: "unable to get fetch user following" });
 
         return res.status(200).json({
             success: true,
