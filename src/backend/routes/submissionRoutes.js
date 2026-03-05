5db80b87a3b0b3776807b69d66df603f0de39ee8
oliveiramnando <oliveiramnando@gmail.com>
2026-03-04 20:16:21 -0500

    tagStrength route

--- src/backend/routes/submissionRoutes.js
+++ src/backend/routes/submissionRoutes.js
@@ -3,6 +3,6 @@ import { tagStrengths } from "../controllers/submissionController.js";
 
 const router = express.Router();
 
-router.get('/strengths', tagStrengths);
+router.get('/strengths', tagStrengths)
 
 export default router;
