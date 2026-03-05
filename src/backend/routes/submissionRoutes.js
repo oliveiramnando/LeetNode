262c40ae5fbed3584d4a6817a20ae4c9443de87a
oliveiramnando <oliveiramnando@gmail.com>
2026-03-05 15:39:34 -0500

    langDistribution route

--- src/backend/routes/submissionRoutes.js
+++ src/backend/routes/submissionRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { tagStrengths } from "../controllers/submissionController.js";
+import { langDistribution, tagStrengths } from "../controllers/submissionController.js";
 
 const router = express.Router();
 
-router.get('/strengths', tagStrengths)
+router.get('/lang-distribution', langDistribution);
+router.get('/strengths', tagStrengths);
 
 export default router;
