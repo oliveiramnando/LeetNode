484649cfaa1aa274d052301196d3f4509abffc52
oliveiramnando <oliveiramnando@gmail.com>
2026-03-04 20:15:25 -0500

    indexing

--- src/backend/models/submissions/lc_submission_events.js
+++ src/backend/models/submissions/lc_submission_events.js
@@ -1,46 +1,47 @@
 import mongoose from "mongoose";
 
-const lc_submission_eventsSchema = mongoose.Schema({
+const lc_submission_eventsSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: "User",
         required: true,
         index: true
     },
     titleSlug: {
         type: String,
         required: true,
-        unique: true,
         index: true
     },
     title: {
-        type: String,
+        type: String
     },
     timeStamp: {
         type: Number,
-        required: true
-    }, 
+        required: true,
+        index: true
+    },
     status: {
         type: String,
         required: true,
+        index: true
     },
     lang: {
         type: String,
         required: true
-    }, 
-},{
+    }
+}, {
     timestamps: true
-})
+});
 
 lc_submission_eventsSchema.index(
-    { userId: 1, titleSlug: 1, timestamp: 1 },
+    { userId: 1, titleSlug: 1, timeStamp: 1 },
     { unique: true }
-)
+);
 
-lc_submission_eventsSchema.index(
-    { userId: 1, status: 1 },
-    { unique: true }
-)
+lc_submission_eventsSchema.index({ 
+    userId: 1, status: 1, titleSlug: 1 
+});
+
 
 export default mongoose.model(
     "lc_submission_events",
