8d14a7666dfd713c48c0f37e170d7fd4b04cc28a
oliveiramnando <oliveiramnando@gmail.com>
2026-03-10 00:52:16 -0400

    submission tracker route

--- src/backend/routes/submissionRoutes.js
+++ src/backend/routes/submissionRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { syncSubmissions, langDistribution, difficultyPerformance, tagStrengths } from "../controllers/submissionController.js";
+import { syncSubmissions, submissionTracker, langDistribution, difficultyPerformance, tagStrengths } from "../controllers/submissionController.js";
 
 const router = express.Router();
 
 router.post('/sync-submissions', syncSubmissions);
 
+router.get('/submission-tracker', submissionTracker);
 router.get('/lang-distribution', langDistribution);
 router.get('/difficulty-performance', difficultyPerformance)
 router.get('/strengths', tagStrengths);
