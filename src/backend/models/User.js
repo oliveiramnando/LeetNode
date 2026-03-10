2f4cd3bacf13fa9c2b5c6568ff1ac6bfd495be94
oliveiramnando <oliveiramnando@gmail.com>
2026-03-10 01:26:33 -0400

    added backed githubID

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+    githubID: {
+        type: Number,
+        index: true
+    },
     githubUsername: {
         type: String,
         trim: true,
