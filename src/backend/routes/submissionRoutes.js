c7d318e532f9590aa57176081fc491ecb7751516
oliveiramnando <oliveiramnando@gmail.com>
2026-03-12 17:32:42 -0400

    removed syn submiissions

--- src/backend/routes/submissionRoutes.js
+++ src/backend/routes/submissionRoutes.js
@@ -1,9 +1,7 @@
 import express from "express";
-import { syncSubmissions, submissionTracker, langDistribution, difficultyPerformance } from "../controllers/submissionController.js";
+import { submissionTracker, langDistribution, difficultyPerformance } from "../controllers/submissionController.js";
 
 const router = express.Router();
-
-router.post('/sync-submissions', syncSubmissions);
 
 router.get('/submission-tracker', submissionTracker);
 router.get('/lang-distribution', langDistribution);
