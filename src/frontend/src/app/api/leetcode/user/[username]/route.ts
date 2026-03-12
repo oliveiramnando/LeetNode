b97265b48433eaf6656b9a8fe55ef21525c60289
oliveiramnando <oliveiramnando@gmail.com>
2026-03-12 17:34:12 -0400

    passed cookies so can sync

--- src/frontend/src/app/api/leetcode/user/[username]/route.ts
+++ src/frontend/src/app/api/leetcode/user/[username]/route.ts
@@ -4,16 +4,24 @@ import { NextResponse } from "next/server";
 type AnyObj = Record<string, any>;
 
 export async function GET(
-  _req: Request,
+  req: Request,
   ctx: { params: Promise<{ username: string }> | { username: string } }
 ) {
-  const { username } = await ctx.params; // ✅ unwrap promise
+  const { username } = await ctx.params;
   const base = process.env.BACKEND_URL || "http://localhost:8080";
+  const cookie = req.headers.get("cookie") ?? "";
+  console.log("NEXT route cookie:", cookie);
 
   try {
     const userRes = await fetch(
       `${base}/api/leetcode/user/${encodeURIComponent(username)}`,
-      { cache: "no-store", headers: { Accept: "application/json" } }
+      {
+        cache: "no-store",
+        headers: {
+          Accept: "application/json",
+          Cookie: cookie,
+        },
+      }
     );
 
     if (!userRes.ok) {
